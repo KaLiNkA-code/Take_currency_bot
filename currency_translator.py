eac5df1c2712adb921315f151209f76e97f927ef
Egor <777egorkalinin777@gmail.com>
2021-10-20 07:46:13 +0300

    Take_currency_tg_bot version 01.01.01 (betta)

---
+++ currency_translator.py
@@ -0,0 +1,5 @@
+"""This part of code is needed for a translator not only into Russian currency"""
+
+
+def check():
+    print("I think, is work!)")
---
+++ parse.py
@@ -0,0 +1,5 @@
+"""This is a part of code for parse currency pages"""
+
+
+def parse():
+    print('I think, it it works')
